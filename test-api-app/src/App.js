2af84e183d55f029a91c085e9c84ca00491b4f0c
jisshub <jissmon476@gmail.com>
2022-10-28 08:58:54 +0530

    Commit All changes

--- test-api-app/src/App.js
+++ test-api-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LeaderBoard from './components/LeaderBoard';
 
 function App() {
   return (
     <div className="App">
-      
+        <LeaderBoard />
     </div>
   );
 }
---
+++ test-api-app/src/components/LeaderBoard.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useState } from 'react'
+import {useFetch} from '../hooks/useFetch.js'
+
+export default function LeaderBoard() {
+    const [url, setUrl] = useState('https://randomuser.me/api?results=10');
+    const [title, setTitle] = useState('')
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [state, setState] = useState('');
+    const [country, setCountry] = useState('');
+    const [picture, setPictire] = useState('');
+    const {data: results}=useFetch(url);    
+    const resultList = results;
+    console.log(resultList);
+    return (
+        <div>
+            <h1>LeaderBoard</h1>
+            {resultList && resultList.map((result) => (
+                <div className='leaderboard'>
+                    <p>
+                        <img src={`${result.picture.large}`}></img>
+                    </p>
+                    <p>
+                        <strong>Name:</strong>&nbsp;
+                        {result.name.title}. {result.name.first} {result.name.last}
+                    </p>
+                    <p>
+                        <strong>Place: </strong>&nbsp;
+                        {result.location.state}, {result.location.country}
+                    </p>
+                    <br></br>
+                    <br></br>
+                </div>
+            ))}
+        </div>
+    )
+}
